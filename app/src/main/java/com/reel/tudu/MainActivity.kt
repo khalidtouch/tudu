d91076bcce80f928f7fcbf748a9b1a55bab820ac
Khalid Isah <khalidhulabaloo9@gmail.com>
2022-06-17 14:44:09 +0100

    bottom nav working sweet

--- app/src/main/java/com/reel/tudu/MainActivity.kt
+++ app/src/main/java/com/reel/tudu/MainActivity.kt
@@ -2,11 +2,24 @@ package com.reel.tudu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.reel.tudu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var _binding:
+    private var _binding: ActivityMainBinding? = null
+    val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.MainNavHostFragment)
+                as NavHostFragment
+        binding?.MainBottomNavigationView?.setupWithNavController(navHostFragment.navController)
+
     }
 }
